1b86395e0cbe4fbf28a19d5de18694b9e26bc189
VishalRaw07 <vishalraw07.co.in@gmail.com>
2023-03-24 12:04:56 +0530

    Feature2(PlayerController-Crouching&jumping) - commit 2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Animator animator;
-
+     
      private void Awake() {
         Debug.Log("PlayerController controller awakes");
     }
@@ -19,9 +19,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         float speed = Input.GetAxisRaw("Horizontal"); //value of this [-1 to 1]
         animator.SetFloat("Speed", Mathf.Abs(speed));
-
     Vector3 scale = transform.localScale;
         if(speed<0)
         {
@@ -32,10 +32,32 @@ public class PlayerController : MonoBehaviour
              scale.x = Mathf.Abs(scale.x);
         }
         transform.localScale = scale;
+
+    //jumping with up arrow key
+    bool jump = Input.GetKeyDown(KeyCode.UpArrow);
+    animator.SetBool("Jump", jump);
+        if(jump == true)
+        {
+             scale.y = Mathf.Abs(scale.y);
+        }
+        transform.localScale = scale;
+
+
+
+    //crouching with leftctrl key
+    bool crouch = Input.GetKeyDown(KeyCode.LeftControl);
+    animator.SetBool("Crouch", crouch);
+    
+        if(crouch == true)
+        {
+             scale.y = Mathf.Abs(scale.y);
+        }
+        transform.localScale = scale;
     }
 
    
     private void OnCollisionEnter2D(Collision2D collision) {
         Debug.Log("Collision: " + collision.gameObject.name);        
     }
+
 }
