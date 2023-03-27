9d1cdd47fde378ce0db569bd627a04ca00841975
VishalRaw07 <vishalraw07.co.in@gmail.com>
2023-03-27 12:10:58 +0530

    Feature3(Movement- vertical Input and jump)- commit 2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,9 +6,12 @@ public class PlayerController : MonoBehaviour
 {
     public Animator animator;
     public float speed;
+    public float jump;
+    private Rigidbody2D rb2d;
 
      private void Awake() {
         Debug.Log("PlayerController controller awakes");
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Start is called before the first frame update
@@ -22,9 +25,21 @@ public class PlayerController : MonoBehaviour
     {
 
         float horizontal = Input.GetAxisRaw("Horizontal"); //value of this [-1 to 1]
-        MoveCharacter(horizontal);
+        float vertical = Input.GetAxisRaw("Jump");
+        MoveCharacter(horizontal, vertical);
+        MoveAnimation(horizontal, vertical);
 
+    
+
+
+    //crouching with leftctrl key
+   
+    }
+
+    private void MoveAnimation(float horizontal, float vertical)
+    {
         animator.SetFloat("Speed", Mathf.Abs(horizontal));
+
         Vector3 scale = transform.localScale;
         if(horizontal<0)
         {
@@ -36,34 +51,34 @@ public class PlayerController : MonoBehaviour
         }
         transform.localScale = scale;
 
-    //jumping with up arrow key
-    bool jump = Input.GetKeyDown(KeyCode.UpArrow);
-    animator.SetBool("Jump", jump);
-        if(jump == true)
-        {
-             scale.y = Mathf.Abs(scale.y);
-        }
-        transform.localScale = scale;
+
+        if(vertical > 0)
+            animator.SetBool("Jump", true);
+        else
+            animator.SetBool("Jump",false);
 
 
-
-    //crouching with leftctrl key
-    bool crouch = Input.GetKeyDown(KeyCode.LeftControl);
-    animator.SetBool("Crouch", crouch);
+        bool crouch = Input.GetKeyDown(KeyCode.LeftControl);
+        animator.SetBool("Crouch", crouch);
     
         if(crouch == true)
         {
              scale.y = Mathf.Abs(scale.y);
         }
         transform.localScale = scale;
+
     }
 
-
-    private void MoveCharacter(float horizontal)
+    private void MoveCharacter(float horizontal, float vertical)
     {
         Vector3 position = transform.position;
         position.x = position.x + horizontal * speed * Time.deltaTime;
         transform.position = position;
+
+        if(vertical > 0)
+        {
+            rb2d.AddForce(new Vector2(0f,jump), ForceMode2D.Force);
+        }
     }
    
     private void OnCollisionEnter2D(Collision2D collision) {
