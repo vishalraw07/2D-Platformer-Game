b7d86d6dca7f6e3f9787ea536bd24e18ffbd3c2e
VishalRaw07 <vishalraw07.co.in@gmail.com>
2023-03-31 12:51:36 +0530

    Feature4-TileMaps(commit 1)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody2D rb2d;
+    private BoxCollider2D boxCollider;
     public Animator animator;
     public float speed;
-    public float jump;
-    private Rigidbody2D rb2d;
+    public float jumpforce;
+     
+    public bool isGrounded;
+    
 
      private void Awake() {
         Debug.Log("PlayerController controller awakes");
         rb2d = gameObject.GetComponent<Rigidbody2D>();
+        boxCollider = gameObject.GetComponent<BoxCollider2D>();
     }
 
     // Start is called before the first frame update
@@ -25,20 +30,22 @@ public class PlayerController : MonoBehaviour
     {
 
         float horizontal = Input.GetAxisRaw("Horizontal"); //value of this [-1 to 1]
-        float vertical = Input.GetAxisRaw("Jump");
+        float vertical = Input.GetAxisRaw("Vertical");
         MoveCharacter(horizontal, vertical);
-        MoveAnimation(horizontal, vertical);
-
-    
-
-
-    //crouching with leftctrl key
+        HorizontalAnimation(horizontal);
+        VerticalAnimation(vertical);
    
     }
 
-    private void MoveAnimation(float horizontal, float vertical)
+   private void MoveCharacter(float horizontal, float vertical)
     {
-        animator.SetFloat("Speed", Mathf.Abs(horizontal));
+        Vector3 position = transform.position;
+        position.x = position.x + horizontal * speed * Time.deltaTime;
+        transform.position = position;
+    }
+    private void HorizontalAnimation(float horizontal)
+    {
+         animator.SetFloat("Speed", Mathf.Abs(horizontal));
 
         Vector3 scale = transform.localScale;
         if(horizontal<0)
@@ -50,10 +57,18 @@ public class PlayerController : MonoBehaviour
              scale.x = Mathf.Abs(scale.x);
         }
         transform.localScale = scale;
+    }
+
+    private void VerticalAnimation(float vertical)
+    {
+         Vector3 scale = transform.localScale;
 
 
-        if(vertical > 0)
+        if(vertical > 0 && isGrounded )
+        {
             animator.SetBool("Jump", true);
+            rb2d.AddForce(new Vector2(0, jumpforce), ForceMode2D.Impulse);
+        }
         else
             animator.SetBool("Jump",false);
 
@@ -69,18 +84,18 @@ public class PlayerController : MonoBehaviour
 
     }
 
-    private void MoveCharacter(float horizontal, float vertical)
-    {
-        Vector3 position = transform.position;
-        position.x = position.x + horizontal * speed * Time.deltaTime;
-        transform.position = position;
-
-        if(vertical > 0)
-        {
-            rb2d.AddForce(new Vector2(0f,jump), ForceMode2D.Force);
-        }
-    }
+ 
    
+   private void OnCollisionStay2D(Collision2D other) 
+   {    
+        if(other.transform.tag== "platform")
+            isGrounded= true; 
+   }
+    private void OnCollisionExit2D(Collision2D other) 
+   {    
+        if(other.transform.tag== "platform")
+            isGrounded= false; 
+   }
     private void OnCollisionEnter2D(Collision2D collision) {
         Debug.Log("Collision: " + collision.gameObject.name);        
     }
